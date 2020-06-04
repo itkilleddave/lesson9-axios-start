090b0d3b9f76bade605569cfbab9b10194ecaf13
Dave Longbottom <davelongbottom@gmail.com>
2020-06-05 07:43:13 +0930

    mapping state

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event</h1>
+    <p>Name: {{ user.name }}</p>
+    <p>Id: {{ user.id }}</p>
+    <h3>Categories</h3>
+    <ul>
+      <li v-for="category in categories" :key="category">
+        {{ category }}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+
+  import { mapState } from 'vuex'
+
+  export default {
+    computed: mapState(['user','categories'])
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
